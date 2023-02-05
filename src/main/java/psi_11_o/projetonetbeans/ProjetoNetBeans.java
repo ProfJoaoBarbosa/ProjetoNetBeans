8369433c59d28776489e1841a40c38e00dbca0ae
João Barbosa <>
2023-02-05 16:19:50 +0000

    Exemplo de Commit local.

---
+++ src/main/java/psi_11_o/projetonetbeans/ProjetoNetBeans.java
@@ -0,0 +1,14 @@
+// Exemplo para criação de repositório com um projeto do NetBeans já existente.
+
+package psi_11_o.projetonetbeans;
+
+/**
+ *
+ * @author João Barbosa
+ */
+public class ProjetoNetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");        
+    }
+}
